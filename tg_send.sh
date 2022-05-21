834e553510f6a6b356488b71823bc508f284ea0d
Georgy Melnikov <gmelnikov@hotmail.com>
2022-05-21 22:30:08 +0300

    tg_send.sh: add parse_mode="markdown" to request

--- tg_send.sh
+++ tg_send.sh
@@ -2,5 +2,5 @@
 # NOTE: ~/.tgrc should have TOKEN and CHAT_ID declared
 . ~/.tgrc
 TEXT="$(cat -)"
-curl -s -X POST "https://api.telegram.org/bot${TOKEN}/sendMessage" -d chat_id="${CHAT_ID}" -d text="${TEXT}" | jq
+curl -s -X POST "https://api.telegram.org/bot${TOKEN}/sendMessage" -d chat_id="${CHAT_ID}" -d text="${TEXT}" -d parse_mode="markdown" | jq
 
